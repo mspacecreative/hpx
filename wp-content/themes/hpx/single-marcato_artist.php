1beed5ad96affde64e78706e8c50652ac2d1b3c1
Matt <matt@mspacecreative.com>
2019-03-19 16:05:55 -0300

    c

--- wp-content/themes/hpx/single-marcato_artist.php
+++ wp-content/themes/hpx/single-marcato_artist.php
@@ -39,9 +39,9 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 							do_action( 'et_before_post' );
 							?>
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'et_pb_post' ); ?>>
+								<h1 class="entry-title"><?php the_title(); ?></h1>
 								<?php if ( ( 'off' !== $show_default_title && $is_page_builder_used ) || ! $is_page_builder_used ) { ?>
 									<div class="et_post_meta_wrapper">
-										<h1 class="entry-title"><?php the_title(); ?></h1>
 			
 									<?php
 										if ( ! post_password_required() ) :
