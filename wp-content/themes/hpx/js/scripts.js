f0c47a6d792ef52bf510b9bba01acb1401527e1b
Matt <matt@mspacecreative.com>
2019-03-20 08:43:04 -0300

    c

--- wp-content/themes/hpx/js/scripts.js
+++ wp-content/themes/hpx/js/scripts.js
@@ -18,10 +18,10 @@ function heroHeight() {
 }
 
 function artistVideos() {
-	var children = $('.artist-videos > li').length
-	
+	var children = $('.artist-videos li').length;
+	var videos = $('.artist-videos li');
 	if (children < 2 ) {
-		$('.artist-videos > li').css('flex-basis', '100%');
+		videos.css('flex-basis', '100%');
 	}
 }
 
