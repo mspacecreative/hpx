18f0b5b8a81a5298f979d8b7d523983a74919ea9
Matt <matt@mspacecreative.com>
2019-04-03 09:03:38 -0300

    c

--- wp-content/plugins/mspace-stickybar/mspace_stickybar.php
+++ wp-content/plugins/mspace-stickybar/mspace_stickybar.php
@@ -28,17 +28,6 @@
  							<?php endwhile; ?>
  						</div>
  						<?php endif; ?>
- 						<?php if( have_rows('button_2', 'options') ): ?>
- 						<div class="sticky-bar-button last">
- 							<?php while( have_rows('button_2', 'options') ): the_row();
- 								$label = get_sub_field('button_label', 'options');
- 								$link = get_sub_field('button_link', 'options'); ?>
- 							<a href="<?php echo $link; ?>" target="_blank">
- 								<?php echo $label; ?>
- 							</a>
- 							<?php endwhile; ?>
- 						</div>
- 						<?php endif; ?>
  					</div>
  				</div>
  			</div>
