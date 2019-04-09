dd7bb2a4b8124334fca0e31e86a29b5c2353f447
Matt <matt@mspacecreative.com>
2019-04-09 15:40:42 -0300

    c

--- wp-content/themes/hpx/includes/sponsor-logos-footer.php
+++ wp-content/themes/hpx/includes/sponsor-logos-footer.php
@@ -6,7 +6,7 @@ if( $images ): ?>
     <ul class="sponsor-logos-list" style="font-size: 0;">
         <?php foreach( $images as $image ): ?>
             <li class="sponsor-logo">
-                <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+                <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
             </li>
         <?php endforeach; ?>
     </ul>
