4a30ad3d3e89bc6b8d384066769e9c1dcc2990e8
Matt <matt@mspacecreative.com>
2019-04-23 08:45:37 -0300

    c

--- wp-content/themes/hpx/includes/sponsor-logos-home.php
+++ wp-content/themes/hpx/includes/sponsor-logos-home.php
@@ -1,12 +1,19 @@
 <?php 
 
 $images = get_field('sponsor_logo_gallery');
+$size = 'medium';
 
 if( $images ): ?>
     <ul class="sponsor-logos-list">
         <?php foreach( $images as $image ): ?>
             <li class="sponsor-logo">
-                <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+				<?php 
+				$squarelogo = get_field('square_logo', $image['ID']); 
+				if ( $squarelogo ):
+                echo wp_get_attachment_image( $image['ID'], $size, "", ["class" => "square-logo"] ); 
+                else : ?>
+                <?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
+                <?php endif; ?>
             </li>
         <?php endforeach; ?>
     </ul>
